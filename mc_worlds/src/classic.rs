8d0ad1623bad745081beb1d4df0d2480e848edc2
Sarah <m.sarahgreywolf@outlook.com>
2020-11-04 20:52:24 +0000

    Set underground of default world to dirt with grass top.

--- mc_worlds/src/classic.rs
+++ mc_worlds/src/classic.rs
@@ -270,7 +270,10 @@ impl ClassicWorld {
         let mut blocks: Vec<u8> =
             (0..(x * y * z)).map(|k| Block::Air.into()).collect();
         // TODO: Split this into multiple threads handling multiple chunks at once and combine at the end
-        for i in 0..x * z * (y/2) {
+        for i in 0..x * z * (y/2) - 1 {
+            blocks[i] = Block::Dirt.into();
+        }
+        for i in 0 * (y/2)..x * z * (y/2) {
             blocks[i] = Block::GrassBlock.into();
         }
 
